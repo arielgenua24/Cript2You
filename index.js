f0a1df5b5c3e81cb7c2cd3bb1580f6062c75063c
ariel <arielgenua24@gmail.com>
2021-07-22 23:21:38 -0300

    implemente js, de manera tal que aplique promesas try/catch para traer la API de binance y colocarle los precios a las criptoMonedas

--- index.js
+++ index.js
@@ -9,20 +9,31 @@ async function getCripto() {
         let btc = data.filter(nombre => nombre.symbol == "BTCUSDT");
         let elements = btc.map(elemento => elemento.price);
         
-        
-        const eth = data.filter(nombre => nombre.symbol == "ETHBTC");
-        ethPrice = 
-        eth.map(elemento2 => elemento2.price); 
-        console.log(ethPrice); 
+        const eth = data.filter(nombre => nombre.symbol == "ETHUSDT");
+        const elementEth = eth.map(elemento2 => elemento2.price); 
     
-        const p = document.getElementById("#P");
-        P.innerText = `este es el precio de ${parseInt(elements)}`;
-    
-    
-    
-    }
+        const bitcoinPrice = document.getElementById("bitcoinPrice");
+        bitcoinPrice.innerText = `${parseInt(elements)}`;
+ 
+        console.log(eth);
 
-    catch (error) {
+        const ethereumPrice = document.getElementById("ethereumPrice");
+        ethereumPrice.innerText = `${parseInt(elementEth)}`;
+
+
+        let ripple = data.filter(nombre => nombre.symbol == "XRPUSDT");
+        let elementsRipple = ripple.map(elemento => elemento.price);
+
+        const ripplePrice = document.getElementById("ripplePrice");
+        ripplePrice.innerText = `${parseInt(elementsRipple)}`;
+
+
+
+
+    
+    
+    
+    } catch (error) {
         console.log("cuidado" + error)
     }
 
